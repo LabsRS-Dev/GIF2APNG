95b6ae932084382108f6c67b7618d859c539773d
iansun <lauer3912@gmail.com>
2017-06-05 14:56:04 +0800

    添加toolsMap 类型处理，完善本地无服务器状态的数据处理工作

--- src/bridge/tools_map.js
+++ src/bridge/tools_map.js
@@ -2,23 +2,36 @@
  * @author Ian
  * @created 2017-04-28 16:48:21
  */
+import { BS } from 'dove.max.sdk'
+
+
+const ToolsType = {
+  WS: 'websocket-server',
+  NTASK: 'native-task'
+}
+
+
 const ToolsMap = {
   // common
-  'get.image.thumb': { cli: 'aiexifcool/common/index', action: 'getImageThumb' },
+  'get.image.thumb': { type: ToolsType.WS, cli: 'aiexifcool/common/index', action: 'getImageThumb' },
 
   // fix
-  'start.fix.image': { cli: 'aiexifcool/fix.image/index', action: 'startFix' },
-  'stop.fix.image': { cli: 'aiexifcool/fix.image/index', action: 'stopFix' },
+  'start.fix.image': { type: ToolsType.WS, cli: 'aiexifcool/fix.image/index', action: 'startFix' },
+  'stop.fix.image': { type: ToolsType.WS, cli: 'aiexifcool/fix.image/index', action: 'stopFix' },
 
   // remove
-  'start.remove.exif': { cli: 'aiexifcool/remove.exif/index', action: 'removeExif' },
-  'stop.remove.exif': { cli: 'aiexifcool/remove.exif/index', action: 'stopRemove' },
+  'start.remove.exif': { type: ToolsType.WS, cli: 'aiexifcool/remove.exif/index', action: 'removeExif' },
+  'stop.remove.exif': { type: ToolsType.WS, cli: 'aiexifcool/remove.exif/index', action: 'stopRemove' },
 
   // modify
-  'get.exif': { cli: 'aiexifcool/edit.image/index', action: 'getExifInfo' },
-  'start.modify.exif': { cli: 'aiexifcool/edit.image/index', action: 'modifyExifInfo' },
-  'stop.modify.exif': { cli: 'aiexifcool/edit.image/index', action: 'stopModify' }
+  'get.exif': { type: ToolsType.WS, cli: 'aiexifcool/edit.image/index', action: 'getExifInfo' },
+  'start.modify.exif': { type: ToolsType.WS, cli: 'aiexifcool/edit.image/index', action: 'modifyExifInfo' },
+  'stop.modify.exif': { type: ToolsType.WS, cli: 'aiexifcool/edit.image/index', action: 'stopModify' },
+
+
+  // ////////////////////////////////////////////////////////////////////////////////////////////////////
+  'gif2apng': { type: ToolsType.NTASK, cli: BS.b$.App.getAppPluginDir() + '/gif2apng', command: [], mainThread: false }
 }
 
 // export
-export { ToolsMap }
+export { ToolsMap, ToolsType }
