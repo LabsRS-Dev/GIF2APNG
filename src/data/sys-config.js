5f5421780a495ecf0a8ac67d173dbe22265e528d
gaogao-li <su3188@163.com>
2017-06-15 10:48:07 +0800

    modify app name

--- src/data/sys-config.js
+++ src/data/sys-config.js
@@ -3,10 +3,10 @@
  * @created 2017-5-31 15:17:21
  */
 const SysConfig = {
-  appName: 'Gif2Apng',
-  version: '3.0.0',
-  homepage: 'https://gmagon.com/products/store/gif2apng/index.htmls',
-  docPage: 'https://gmagon.com/products/store/gif2apng/docs.html',
+  appName: 'Gmagon Gif',
+  version: '3.0',
+  homepage: 'https://gmagon.com/products/store/gmagon_gif/index.htmls',
+  docPage: 'https://gmagon.com/products/store/gamgon_gif/docs.html',
   companyName:'Gmagon',
   appReleaseDate:'2017/06/10',
   companyWebsiteHomepage:'https://gmagon.com',
