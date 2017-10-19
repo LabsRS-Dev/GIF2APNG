d0be16ed3bf989e7999d38adf844a2c13d94f123
gaogao-li <su3188@163.com>
2017-10-19 09:09:40 +0800

    修改了一些BUG/调整了下侧边栏状态（17/10/19）

--- src/routes.js
+++ src/routes.js
@@ -4,29 +4,29 @@ import IconsRef from './data/icon.js'
 import { SysConfig } from './data/sys-config.js'
 // /GIF2APNG
 import WelcomePage from './pages/GIF2APNG/Welcome.vue'
-import DiscoverPage from './pages/GIF2APNG/Find/Discover.vue'
+// import DiscoverPage from './pages/GIF2APNG/Find/Discover.vue'
 import ConvertPage from './pages/GIF2APNG/Convert.vue'
 import ResizePage from './pages/GIF2APNG/Resize/Resize'
 import Resize_Single from './pages/GIF2APNG/Resize/Resize_Single'
 import Resize_Batch from './pages/GIF2APNG/Resize/Resize_Batch'
 
 // /
-import Recommendation from './pages/GIF2APNG/Find/recommendation.vue'
-import Classification from './pages/GIF2APNG/Find/classification.vue'
-import Material from './pages/GIF2APNG/Find/material.vue'
-import TopRank from './pages/GIF2APNG/Find/topRank.vue'
-import ImageList from './pages/GIF2APNG/Find/ImageList.vue'
-import ImgFileList from './pages/GIF2APNG/Find/imgFileList.vue'
-import Discuss from './pages/GIF2APNG/Find/discuss.vue'
-import Collector from './pages/GIF2APNG/Find/collector.vue'
+// import Recommendation from './pages/GIF2APNG/Find/recommendation.vue'
+// import Classification from './pages/GIF2APNG/Find/classification.vue'
+// import Material from './pages/GIF2APNG/Find/material.vue'
+// import TopRank from './pages/GIF2APNG/Find/topRank.vue'
+// import ImageList from './pages/GIF2APNG/Find/ImageList.vue'
+// import ImgFileList from './pages/GIF2APNG/Find/imgFileList.vue'
+// import Discuss from './pages/GIF2APNG/Find/discuss.vue'
+// import Collector from './pages/GIF2APNG/Find/collector.vue'
 // /
-import Seek from './pages/GIF2APNG/Search/Seek.vue'
-import Single from './pages/GIF2APNG/Search/Single.vue'
-import Album from './pages/GIF2APNG/Search/Album.vue'
+// import Seek from './pages/GIF2APNG/Search/Seek.vue'
+// import Single from './pages/GIF2APNG/Search/Single.vue'
+// import Album from './pages/GIF2APNG/Search/Album.vue'
 // / /
-import Download from './pages/GIF2APNG/Down/Download.vue'
-import Download_Single from './pages/GIF2APNG/Down/download_Single.vue'
-import Download_Album from './pages/GIF2APNG/Down/download_Album.vue'
+// import Download from './pages/GIF2APNG/Down/Download.vue'
+// import Download_Single from './pages/GIF2APNG/Down/download_Single.vue'
+// import Download_Album from './pages/GIF2APNG/Down/download_Album.vue'
 // Config SystemConfig
 const icons = IconsRef.iconSet
 const rootPath = '/' + SysConfig.appName
@@ -51,73 +51,74 @@ const menu = [{
       tipAsSubTitle: true,
       icon: icons.adjust,
       sourceUrl: ''
-    }, {
-      path: rootPath + '/Find',
-      show: true,
-      component: DiscoverPage,
-      title: 'routes.common.menu.discover.title',
-      tip: 'routes.common.menu.discover.tip',
-      tipAsSubTitle: true,
-      icon: icons.adjust,
-      sourceUrl: '',
-      redirect: { name: 'Recommendation' },
-      children: [
-        {
-          name: 'Recommendation',
-          tipAsSubTitle: true,
-          tip: 'routes.common.menu.discover.tip',
-          path: 'recommendation',
-          component: Recommendation
-        },
-        {
-          path: 'classification',
-          component: Classification
-        },
-        {
-          path: 'material',
-          component: Material
-        },
-        {
-          path: 'topRank',
-          component: TopRank
-        }
-      ]
     },
+    // {
+    //   path: rootPath + '/Find',
+    //   show: true,
+    //   component: DiscoverPage,
+    //   title: 'routes.common.menu.discover.title',
+    //   tip: 'routes.common.menu.discover.tip',
+    //   tipAsSubTitle: true,
+    //   icon: icons.adjust,
+    //   sourceUrl: '',
+    //   redirect: { name: 'Recommendation' },
+    //   children: [
+    //     {
+    //       name: 'Recommendation',
+    //       tipAsSubTitle: true,
+    //       tip: 'routes.common.menu.discover.tip',
+    //       path: 'recommendation',
+    //       component: Recommendation
+    //     },
+    //     {
+    //       path: 'classification',
+    //       component: Classification
+    //     },
+    //     {
+    //       path: 'material',
+    //       component: Material
+    //     },
+    //     {
+    //       path: 'topRank',
+    //       component: TopRank
+    //     }
+    //   ]
+    // },{
+    //   path: rootPath + '/Find/ImageList/:id',
+    //   show: false,
+    //   component: ImageList,
+    //   children: [
+    //     {
+    //       name: 'ImageList',
+    //       path: 'imgFileList',
+    //       component: ImgFileList
+    //     },
+    //     {
+    //       path: 'discuss',
+    //       component: Discuss
+    //     },
+    //     {
+    //       path: 'collector',
+    //       component: Collector
+    //     }
+    //   ]
+    // }, {
+    //   path: rootPath + '/Search',
+    //   show: false,
+    //   component: Seek,
+    //   children: [
+    //     {
+    //       name: 'Single',
+    //       path: 'Single',
+    //       component: Single
+    //     }, {
+    //       name: 'Album',
+    //       path: 'Album',
+    //       component: Album
+    //     }
+    //   ]
+    // },
     {
-      path: rootPath + '/Find/ImageList/:id',
-      show: false,
-      component: ImageList,
-      children: [
-        {
-          name: 'ImageList',
-          path: 'imgFileList',
-          component: ImgFileList
-        },
-        {
-          path: 'discuss',
-          component: Discuss
-        },
-        {
-          path: 'collector',
-          component: Collector
-        }
-      ]
-    }, {
-      path: rootPath + '/Search',
-      show: false,
-      component: Seek,
-      children: [
-        {
-          name: 'Single',
-          path: 'Single',
-          component: Single
-        }, {
-          name: 'Album',
-          path: 'Album',
-          component: Album
-        }
-      ]
-    }, {
       path: rootPath + '/convert',
       show: true,
       component: ConvertPage,
@@ -145,28 +146,29 @@ const menu = [{
       name: 'Resize_Batch',
       show: false,
       component: Resize_Batch
-    }, {
-      path: rootPath + '/Down',
-      show: true,
-      component: Download,
-      title: 'routes.common.menu.download.title',
-      tip: 'routes.common.menu.download.tip',
-      tipAsSubTitle: true,
-      icon: icons.adjust,
-      sourceUrl: '',
-      redirect: { name: 'Download_Single' },
-      children: [
-        {
-          name: 'Download_Single',
-          path: 'Download_Single',
-          component: Download_Single
-        }, {
-          name: 'Download_Album',
-          path: 'Download_Album',
-          component: Download_Album
-        }
-      ]
     }
+    // ,{
+    //   path: rootPath + '/Down',
+    //   show: true,
+    //   component: Download,
+    //   title: 'routes.common.menu.download.title',
+    //   tip: 'routes.common.menu.download.tip',
+    //   tipAsSubTitle: true,
+    //   icon: icons.adjust,
+    //   sourceUrl: '',
+    //   redirect: { name: 'Download_Single' },
+    //   children: [
+    //     {
+    //       name: 'Download_Single',
+    //       path: 'Download_Single',
+    //       component: Download_Single
+    //     }, {
+    //       name: 'Download_Album',
+    //       path: 'Download_Album',
+    //       component: Download_Album
+    //     }
+    //   ]
+    // }
   ]
 },
 {
