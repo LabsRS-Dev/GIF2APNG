e40bdebc0c1ff527fad0e24d00f13fde7edc02df
iansun <lauer3912@gmail.com>
2017-12-01 16:07:34 +0800

    增加可扩展参数SysConfig部分，适应可变品牌及产品名称策略

--- src/data/sys-config.js
+++ src/data/sys-config.js
@@ -1,17 +1,58 @@
+import {
+  BS,
+  Observable,
+  Util,
+  _
+} from 'dove.max.sdk'
 /**
  * @author Ian
  * @created 2017-5-31 15:17:21
  */
+
+const internal = {
+  getAppName: () => {
+    return BS.b$.App.getAppName()
+  },
+  getAppVersion: () => {
+    return BS.b$.App.getAppVersion()
+  },
+  getHomePage: () => {
+    return BS.b$.App.getHomePageURL()
+  },
+  getDocPage: () => {
+    return BS.b$.App.getDocumentPageURL()
+  },
+  getNewsDataUrl: () => {
+    return BS.b$.App.getNewsJsonURL()
+  },
+  getVendorName: () => {
+    return BS.b$.App.getVendorName()
+  },
+  getVendorWebSiteURL: () => {
+    return BS.b$.App.getVendorWebSiteURL()
+  },
+  getAdditionalConfigURL: () => {
+    return BS.b$.App.getAdditionalConfigURL()
+  },
+  getSupportPageURL: () => {
+    return BS.b$.App.getSupportPageURL()
+  },
+  getSupportEmail: () => {
+    return BS.b$.App.getSupportEmail()
+  }
+}
+
+
 const SysConfig = {
-  appName: 'GMagon Gif',
-  version: '2.0.0',
-  homepage: 'https://gmagon.com/products/store/gmagon_gif/index.html',
-  docPage: 'https://gmagon.com/products/store/gamgon_gif/docs.html',
-  newsDataUrl: 'https://gmagon.com/products/store/gmagon_gif/data/news.json',
-  companyName: 'GMagon',
-  appReleaseDate: '2017/06/16',
-  companyWebsiteHomepage: 'https://gmagon.com',
-  support: 'http://gmagon.com/support',
+  appName: internal.getAppName() || 'GMagon GIF',
+  version: internal.getAppVersion() || '20171210.10.35',
+  homepage: internal.getHomePage() || 'https://gmagon.com/products/store/gmagon_gif/index.html',
+  docPage: internal.getDocPage() || 'https://gmagon.com/products/store/gamgon_gif/docs.html',
+  newsDataUrl: internal.getNewsDataUrl() || 'https://gmagon.com/products/store/gmagon_gif/data/news.json',
+  companyName: internal.getVendorName() || 'GMagon',
+  appReleaseDate: '2017/12/10',
+  companyWebsiteHomepage: internal.getVendorWebSiteURL() || 'https://gmagon.com',
+  support: internal.getSupportPageURL() || 'http://gmagon.com/support',
   // apiHost: 'https://api.gmagon.com/plugin/gif/api',
   apiHost: 'http://192.168.3.6:5000/plugin/gif/api',
   // userMachine: 'https://api.gmagon.com/api',
